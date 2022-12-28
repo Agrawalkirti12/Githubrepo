b470965cc6028d956de348177013ab3cad1b7e8e
kirti Agrawal <kirti.agrawal02@nagarro.com>
2022-12-28 17:00:04 +0530

    first commit

---
+++ LampCode.java
@@ -0,0 +1,41 @@
+package com.kirti;
+
+public class LampCode {
+
+			  // stores the value for light
+			  // true if light is on
+			  // false if light is off
+			  boolean isOn;
+
+			  // method to turn on the light
+			  void turnOn() {
+			    isOn = true;
+			    System.out.println("Light on? " + isOn);
+
+			  }
+
+			  // method to turnoff the light
+			  void turnOff() {
+			    isOn = false;
+			    System.out.println("Light on? " + isOn);
+			  }
+			}
+
+			class Main {
+			  public static void main(String[] args) {
+
+			    // create objects led and halogen
+				  LampCode led = new LampCode();
+				  LampCode halogen = new LampCode();
+
+			    // turn on the light by
+			    // calling method turnOn()
+			    led.turnOn();
+
+			    // turn off the light by
+			    // calling method turnOff()
+			    halogen.turnOff();
+			  }
+			
+
+}
